6bb7af0e552bea566164b625179f5d4cbb279204
BigDog <hpb25200@163.com>
2021-12-02 21:37:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 3dB
+ * @creat 2021-12-02 21:36
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
